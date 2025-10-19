9979d4ad596d9a6e2adfa199a1ccf792c026013a
ievgen lysenko <lisiyjeka@gmail.com>
2025-10-19 21:11:35 +0300

    homwork#4

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-name = input("enter your name please: ")
-message_of_app = f"Так це ж наш новий студент, {name} !"
-print (message_of_app)    
+def factorial(n):
+    # Функція для обчислення факторіала числа n
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+def number_of_groups(n, k):
+    # Використовуємо формулу сполучень без повторень
+    return factorial(n) // (factorial(k) * factorial(n - k))
+
+# Тестуємо функцію
+n = 50  # кількість підписників
+k = 7   # кількість переможців
+
+result = number_of_groups(n, k)
+print(result)  # Це виведе кількість можливих різних списків переможців
